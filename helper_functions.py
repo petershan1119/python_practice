34431cad8e5bb9836b7855a7acdaee57f26de085
Sang Won Han <peter.s.han.1119@gmail.com>
2018-01-26 10:28:01 +0900

    헬퍼함수들 모음

---
+++ helper_functions.py
@@ -0,0 +1,23 @@
+from itertools import combinations, chain, tee
+
+
+def flatten(list_of_lists):
+    """
+    목적: 중첩된 list를 평평하게 만들어주고 iterator로 뱐환
+    예제:
+    a = [[1,2,3],[4,5,6,7],[8,9]]
+    list(flatten(a)) -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
+    return chain.from_iterable(list_of_lists)
+
+
+def pairwise(iterable):
+    """
+    목적: 리스트를 연속된 쌍으로 만들어 tuple로 묶은 뒤 iterator 반환
+    예제:
+    a = [1,2,3,4,5,6]
+    list(pairwise(a)) -> [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
